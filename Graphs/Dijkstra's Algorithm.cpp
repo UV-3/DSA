f6cdc992ba4b766db37c85d0f6caf81ec731a699
Yuvraj Neema <68850543+UV-3@users.noreply.github.com>
2021-07-02 03:11:47 +0530

    Update Dijkstra's Algorithm.cpp

--- Graphs/Dijkstra's Algorithm.cpp
+++ Graphs/Dijkstra's Algorithm.cpp
@@ -1,21 +1,21 @@
 const int N=2e5+5;
 
-vector<pair<int,int>>adj[N+1];
-vector<int>d(N,INT_MAX);
+vector<pair<int,int>>adj[N];
+vector<int>dis(N,LLONG_MAX),vis(N,0);
 
 void dijkstra(int s){
     priority_queue< pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>> >q;
-    d[s]=0;
+    dis[s]=0;
     q.push({0,s});
     while(!q.empty()){
-        int v= q.top().second;
+        int u=q.top().second;
         q.pop();
-        for(auto x:adj[v]){
-            int dist= x.second;
-            int u= x.first;
-            if(d[u]> d[v]+dist){
-                d[u]=d[v]+dist;
-                q.push({d[u],u});
+        if(vis[u]) continue;
+        vis[u]=1;
+        for(auto [v,w]:adj[u]){
+            if(dis[v]>dis[u]+w){
+                dis[v]=dis[u]+w;
+                q.push({dis[v],v});
             }
         }
     }
