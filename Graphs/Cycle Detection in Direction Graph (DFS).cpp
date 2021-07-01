4497d182f2a26d12d9e6942ab5537cb5949ec489
Yuvraj Neema <68850543+UV-3@users.noreply.github.com>
2021-07-01 23:49:10 +0530

    Update Cycle Detection in Direction Graph (DFS).cpp

--- Graphs/Cycle Detection in Direction Graph (DFS).cpp
+++ Graphs/Cycle Detection in Direction Graph (DFS).cpp
@@ -1,9 +1,12 @@
+// LOGIC-> dfsvis marks those nodes which are curently present in the dfs call as soon as
+// dfs call is over dfsvis is unmarked 
+// cycle exists when an adjacent node of a node is visited and is also present in dfs call (dfsvis) 
 const int N=2e5+5;
 
 vector<int>adj[N];
 vector<bool>vis(N,false);
 vector<bool>dfsvis(N,false);
-// basic is if for a node both before going on that node if both vis and dfsvis are true then there is a cycle
+
 bool dfs(int v){
     vis[v]=true;
     dfsvis[v]=true;
